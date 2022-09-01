0c3068368ca9c8c9b8be043360cea2a519084ab6
danielcrubens <dcr.tecnologi@gmail.com>
2022-09-01 11:09:28 -0300

    tipos de saida  :white_check_mark:

---
+++ js/tipos-saida.js
@@ -0,0 +1,11 @@
+//Caixa de alerta para exibir dados 
+alert(5+5)
+
+//Dados de exibição" em HTML, (na maioria dos casos), você irá definir o valor de um innerHTML propriedade
+document.querySelector("#texto").innerHTML = "Texto com <b>JS</b>"
+
+//Para fins de teste
+document.write('Escrito com document.write')
+
+//No navegador, para exibir dados
+console.log('Exibir no navegador')
