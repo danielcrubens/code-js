7803e3d97aa1f2114839870e2fa5624b1adb63e4
danielcrubens <dcr.tecnologi@gmail.com>
2022-09-01 15:01:46 -0300

    variáveis  :white_check_mark:

---
+++ js/variavel.js
@@ -0,0 +1,6 @@
+// var - a variável pode ser redeclarada
+// let - não pode ser redeclarada uma variavel (só funciona dentro do escopo que ele está trabalhando)
+// const - nunca vai mudar
+
+let carName = "Volvo"
+document.querySelector('#texto').innerHTML = carName
