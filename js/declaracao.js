6041246bbc64167ba8a7434e49a42c0ae35c75e6
danielcrubens <dcr.tecnologi@gmail.com>
2022-09-01 11:12:42 -0300

    declaração  :white_check_mark:

---
+++ js/declaracao.js
@@ -0,0 +1,14 @@
+
+//Declaração
+
+let bloco = document.querySelector('#declaracao')
+
+bloco.addEventListener('click',minhaFuncao)
+
+
+//Declaraçãoem bloco
+function minhaFuncao(){
+document.querySelector('#texto1').innerHTML ="Declaração em bloco <u>Texto1 pelo JS</u>"
+document.querySelector('#texto2').innerHTML ="Declaração em bloco <u>Texto2 pelo JS</u>"
+
+}
