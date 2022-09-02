b142c3be930ef592fb85bb30f968a09fffbb8b0b
danielcrubens <dcr.tecnologi@gmail.com>
2022-09-02 10:04:27 -0300

    funções  :white_check_mark:

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+/*  FUNÇÔES 
+
+Uma função JavaScript é  um bloco de código projetado para executar uma tarefa específica
+
+É como uma pequena "fabrica" onde você faz  uma entrada e ele dá uma saída
+
+Pode ser encarado como "mini-programas" projetados para fazer uma tarefa que vai contribuir para todo código
+
+Uma função JavaScript é exacutada quando "algo a invoca" (chama-a)
+
+*/
+
+var x = minhaFuncao(4, 3);        
+
+function minhaFuncao(a, b) {
+    return a * b;                
+}
